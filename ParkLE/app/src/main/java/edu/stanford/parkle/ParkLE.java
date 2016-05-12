946d9298e809e6239acc27f033a7320edc93afa1
Antonino Abundes <nabundes@lithiumstart.com>
2016-05-12 15:39:00 -0700

    Changed the modifiers of the variables in the ParkLE application class from protected to package which is the default. It makes more sense that way.`

--- ParkLE/app/src/main/java/edu/stanford/parkle/ParkLE.java
+++ ParkLE/app/src/main/java/edu/stanford/parkle/ParkLE.java
@@ -18,31 +18,34 @@ import java.util.Map;
  */
 public class ParkLE extends Application {
 
-    protected static final long ALARM_INTERVAL = 20*1000;
-    protected static final long SCAN_PERIOD = 2*1000;
+    // All constants and global variables are purposely declared here without a privacy modifier
+    // because they are intended to be visible to the entire package which is the default.
 
-    protected static final String INTENT_ACTION_CHECK_BEACON = "edu.parkle.CHECK_BEACON";
+    static final long ALARM_INTERVAL = 20*1000;
+    static final long SCAN_PERIOD = 2*1000;
 
-    protected static final String USER_INFO = "userInfo";
-    protected static final String CAR_STATE_INFO = "CURRENT_STATE" ;
+    static final String INTENT_ACTION_CHECK_BEACON = "edu.parkle.CHECK_BEACON";
+
+    static final String USER_INFO = "userInfo";
+    static final String CAR_STATE_INFO = "CURRENT_STATE" ;
 //    protected static final String BEACON_STATE_INFO = "BEACON_STATE";
-    protected static final String BEACON_ADDRESS_INFO = "BEACON_ADDRESS";
+    static final String BEACON_ADDRESS_INFO = "BEACON_ADDRESS";
 //    protected static final String CAR_MODULE_STATE_INFO = "carModuleState";
 
-    protected static final String CAR_MODULE_ADDRESS = "E9:40:B9:B9:C0:05";
-    protected static final String LOT_A_BEACON_ADDRESS = "FA:AD:C0:21:19:3C";
-    protected static final String LOT_B_BEACON_ADDRESS = "C8:05:62:94:0C:10";
-    protected static final String LOT_C_BEACON_ADDRESS = "D4:85:90:D6:9A:CD";
+    static final String CAR_MODULE_ADDRESS = "E9:40:B9:B9:C0:05";
+    static final String LOT_A_BEACON_ADDRESS = "FA:AD:C0:21:19:3C";
+    static final String LOT_B_BEACON_ADDRESS = "C8:05:62:94:0C:10";
+    static final String LOT_C_BEACON_ADDRESS = "D4:85:90:D6:9A:CD";
 
-    protected static final int CAR_NOT_IN_LOT = 0;
-    protected static final int CAR_IDLE_IN_LOT = 1;
-    protected static final int CAR_PARKED_IN_LOT = 2;
-    protected static final int NOT_CONNECTED = 0;
-    protected static final int CONNECTED = 1;
+    static final int CAR_NOT_IN_LOT = 0;
+    static final int CAR_IDLE_IN_LOT = 1;
+    static final int CAR_PARKED_IN_LOT = 2;
+    static final int NOT_CONNECTED = 0;
+    static final int CONNECTED = 1;
 
-    protected static SharedPreferences sharedPreferences;
+    static SharedPreferences sharedPreferences;
 
-    protected static final Map<String, String> lotNames = new HashMap<>();
+    static final Map<String, String> lotNames = new HashMap<>();
 
     @Override
     public void onCreate() {
